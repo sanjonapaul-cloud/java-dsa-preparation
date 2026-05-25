9b6efa421a306a7e2c9793eb58365f39626d0120
Sanjona Paul <sanjonapaul@gmail.com>
2026-05-25 09:01:59 +0530

    Added array basics

---
+++ 01_Arrays/00_Notes/Notes.md
@@ -0,0 +1,14 @@
+# Arrays Notes
+
+## Traversal
+Used to visit every element.
+
+Time Complexity:
+O(n)
+
+---
+
+# Common Patterns
+- Two Pointers
+- Sliding Window
+- Prefix Sum
---
+++ 01_Arrays/01_Basics/ArrayTraversal.java
@@ -0,0 +1,11 @@
+public class ArrayTraversal {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        for(int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
